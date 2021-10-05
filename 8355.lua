cd022695c3d4de16d25c7069df533ae77d14253b
Tolga Öztürk <91906149+Muco-op@users.noreply.github.com>
2021-10-05 23:55:48 +0300

    Add files via upload

---
+++ 8355.lua
@@ -0,0 +1,494 @@
+gg.alert("137372")
+gg.alert("273372")
+
+local L0_1027, L1_1028
+L0_1027 = "💎  THIS IS OUR SELF ENCRYPTION BY VMGAMING 💎"
+L1_1028 = {}
+function L1_1028.Q()
+  local L0_1029, L1_1030, L2_1031, L3_1032, L4_1033, L5_1034, L6_1035, L7_1036, L8_1037, L9_1038, L10_1039, L11_1040, L12_1041, L13_1042, L14_1043, L15_1044, L16_1045, L17_1046, L18_1047, L19_1048, L20_1049, L21_1050, L22_1051, L23_1052, L24_1053, L25_1054, L26_1055, L27_1056, L28_1057, L29_1058, L30_1059, L31_1060, L32_1061, L33_1062, L34_1063, L35_1064, L36_1065, L37_1066, L38_1067, L39_1068, L40_1069, L41_1070, L42_1071, L43_1072, L44_1073, L45_1074, L46_1075, L47_1076, L48_1077, L49_1078, L50_1079, L51_1080, L52_1081, L53_1082, L54_1083, L55_1084, L56_1085, L57_1086, L58_1087, L59_1088, L60_1089, L61_1090, L62_1091, L63_1092, L64_1093, L65_1094, L66_1095, L67_1096, L68_1097, L69_1098, L70_1099, L71_1100, L72_1101, L73_1102, L74_1103, L75_1104, L76_1105, L77_1106, L78_1107, L79_1108, L80_1109, L81_1110, L82_1111, L83_1112, L84_1113, L85_1114, L86_1115, L87_1116, L88_1117, L89_1118, L90_1119, L91_1120, L92_1121, L93_1122, L94_1123, L95_1124, L96_1125, L97_1126, L98_1127, L99_1128, L100_1129, L101_1130, L102_1131, L103_1132, L104_1133, L105_1134, L106_1135, L107_1136, L108_1137, L109_1138, L110_1139, L111_1140, L112_1141, L113_1142, L114_1143, L115_1144, L116_1145, L117_1146, L118_1147, L119_1148, L120_1149, L121_1150, L122_1151, L123_1152, L124_1153, L125_1154, L126_1155, L127_1156, L128_1157
+   function setvalue(A0_1158, A1_1159, A2_1160)
+    local L3_1161
+    L3_1161 = {}
+    L3_1161[1] = {}
+    L3_1161[1].address = A0_1158
+    L3_1161[1].flags = A1_1159
+    L3_1161[1].value = A2_1160
+    gg.setValues(L3_1161)
+  end
+gg.alert("ANONYMODX SCRİPT GÜNCELLENİYOR...","HEMEN GÜNCELLE")
+  gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("578351706144768;564058054983680")
+gg.refineNumber("578351706144768")
+gg.getResults(50000)
+gg.editAll("288233678981562368", gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("577252194516992;288233678981562368")
+gg.refineNumber("577252194516992")
+gg.getResults(50000)
+gg.editAll("936753184964083712", gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("360287995959443456")
+gg.refineNumber("360287995959443456")
+gg.getResults(50000)
+gg.editAll("936753184964083712", gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("360287987369508864")
+gg.refineNumber("360287987369508864")
+gg.getResults(50000)
+gg.editAll("936753184964083712", gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("133634;133890")
+gg.refineNumber("133634")
+gg.getResults(50000)
+gg.editAll("131586", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("144387;262403")
+gg.refineNumber("144387")
+gg.getResults(50000)
+gg.editAll("133890", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("134914;144387")
+gg.refineNumber("134914")
+gg.getResults(50000)
+gg.editAll("133890", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("133888")
+gg.refineNumber("133888")
+gg.getResults(50000)
+gg.editAll("361418272522109953", gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("262400")
+gg.refineNumber("262400")
+gg.getResults(50000)
+gg.editAll("361418272522109953", gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("131585")
+gg.refineNumber("131585")
+gg.getResults(50000)
+gg.editAll("361418272522109953", gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("280~590336;67109633")
+gg.refineNumber("280~590336")
+gg.getResultsCount()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("134914;144387", gg.TYPE_DWORD)
+gg.refineNumber("134914", gg.TYPE_DWORD)
+gg.getResults(62877)
+gg.editAll("133890", gg.TYPE_DWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("133634;133890", gg.TYPE_DWORD)
+gg.refineNumber("133634", gg.TYPE_DWORD)
+gg.getResults(62877)
+gg.editAll("133890", gg.TYPE_DWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("133634;134658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("133634", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(62877)
+gg.editAll("133890", gg.TYPE_DWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("909391408", gg.TYPE_DWORD)
+gg.getResults(62877)
+gg.editAll("1089886885", gg.TYPE_DWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("909391408", gg.TYPE_DWORD)
+gg.getResults(999)
+gg.editAll("1089886885", gg.TYPE_DWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("67633927;67109377", gg.TYPE_DWORD)
+gg.refineNumber("67633927;67109377", gg.TYPE_DWORD)
+gg.getResults(50000)
+gg.editAll("67633927", gg.TYPE_DWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("67109390;67114093", gg.TYPE_DWORD)
+gg.refineNumber("67109390;67114093", gg.TYPE_DWORD)
+gg.getResults(50000)
+gg.editAll("67633927", gg.TYPE_DWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("196,864;16,842,753::5", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("196,864", gg.TYPE_DWORD)
+gg.getResults(50000)
+gg.editAll("84,149,249", gg.TYPE_DWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("67,109,377;16,384", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("16384", gg.TYPE_DWORD)
+gg.getResults(50000)
+gg.editAll("84,149,249", gg.TYPE_DWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("131,330;134,658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("134,658", gg.TYPE_DWORD)
+gg.getResults(50000)
+gg.editAll("84,149,249", gg.TYPE_DWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("135,682;144,387", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("135,682", gg.TYPE_DWORD)
+gg.getResults(50000)
+gg.editAll("84,149,249", gg.TYPE_DWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("4,096;135,682", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("4,096", gg.TYPE_DWORD)
+gg.getResults(50000)
+gg.editAll("84,149,249", gg.TYPE_DWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("134,402;67,109,377", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("134,402", gg.TYPE_DWORD)
+gg.getResults(50000)
+gg.editAll("84,149,249", gg.TYPE_DWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("134,658;134,658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("134,658", gg.TYPE_DWORD)
+gg.getResults(50000)
+gg.editAll("84,149,249", gg.TYPE_DWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("131,842;133,635", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("131,842", gg.TYPE_DWORD)
+gg.getResults(50000)
+gg.editAll("84,149,249", gg.TYPE_DWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("133,634;135,682", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("133,634", gg.TYPE_DWORD)
+gg.getResults(50000)
+gg.editAll("84,149,249", gg.TYPE_DWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("133,378;144,387", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("133,378", gg.TYPE_DWORD)
+gg.getResults(50000)
+gg.editAll("84,149,249", gg.TYPE_DWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("620137442967552")
+gg.refineNumber("620137442967552")
+gg.getResults(510)
+gg.editAll("288233678981562368", gg.TYPE_QWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("620137442967552")
+gg.refineNumber("620137442967552")
+gg.getResults(5010)
+gg.editAll("288233678981562368", gg.TYPE_QWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("578351706144768;564058054983680")
+gg.refineNumber("578351706144768")
+gg.getResults(50010)
+gg.editAll("288233678981562368", gg.TYPE_QWORD)
+gg.searchNumber("577252194516992;288233678981562368")
+gg.refineNumber("577252194516992")
+gg.getResults(50010)
+gg.editAll("288233678981562368", gg.TYPE_QWORD)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("3298534883328~288233678981562368;288233678981562368")
+gg.refineNumber("3298534883328~288233678981562368")
+gg.sleep(300)
+gg.alert("GÜNCELLEME SONA ERDİ ! ")
+  function HOME()
+    PRO = gg.multiChoice({
+      "🎯 AIMBOT 🎯",
+      "📺 IPAD VIEW 📺",
+      "💥 LESS RECOIL 💥",
+      "🤯 HEADSHOT 🤯",
+      "➕ SMALL CROSHIER ➕",
+      "✴️ NO RECOIL ✴️",
+      "☄️ FAST SHOOT ☄️",
+      "🦘 LONG JUMP 🦘",
+      "🥾 SPEED KNOCK ON 🥾",
+      "🥾 SPEED KNOCK OFF 🥾",
+      "🏜️ DESERT MAP 🏜️",
+      "🌌 BLACK SKY 🌌",
+      "🚔 CARS SPEED 🚔",
+      "⛳ NO GRASS ⛳",
+      "🌫️ NO FOG 🌫️️",
+      "☸️ AIMLOCK ☸️",
+      "✨ MAGIC BULLET ✨",
+      "💣 INSTANT HIT 💣",
+      "🌉 NIGHT MODE 🌉",
+      "❌ X HIT EFFECT ❌",
+      "🏃‍♀️️💨 FLASH ON ☑️",
+      "🏃‍♀️💨 FLASH OFF ❎",
+      "✖️✖️ 𝙴𝚇𝙸𝚃 ✖️✖️"
+    }, nil, (os.date("• Cᴏᴘʏʀɪɢʜᴛ © 2021 ✨ @ANONYMODX ✨ \n┣ 🕐 %H:%M[🗓%h%d]")))
+    if PRO == nil then
+    else
+      if PRO[1] == true then
+        AIM()
+      end--
+      if PRO[2] == true then
+        IPAD()
+      end--
+      if PRO[3] == true then
+        LESS()
+      end--
+      if PRO[4] == true then
+        HEAD()
+      end--
+      if PRO[5] == true then
+        SMALL()
+      end--
+      if PRO[6] == true then
+        ANTI()
+      end--
+      if PRO[7] == true then
+        LONG()
+      end--
+      if PRO[8] == true then
+        JUMP()
+      end--
+      if PRO[9] == true then
+        KNOCK()
+      end--
+      if PRO[10] == true then
+        KNOCKOFF()
+      end--
+      if PRO[11] == true then
+        DESERT()
+      end--
+      if PRO[12] == true then
+        BLACK()
+      end--
+      if PRO[13] == true then
+        PRONE()
+      end--
+      if PRO[14] == true then
+        GRASS()
+      end--
+      if PRO[15] == true then
+        FOG()
+      end--
+      if PRO[16] == true then
+        AIMLOCK()
+      end--
+      if PRO[17] == true then
+        MAGIC()
+      end--
+      if PRO[18] == true then
+        INSTANT()
+      end--
+      if PRO[20] == true then
+        XHIT()
+      end--
+      if PRO[21] == true then
+        FASTS()
+      end--
+      if PRO[22] == true then
+        CARSP()
+      end--
+      if PRO[23] == true then
+        MO()
+      end--
+    end--
+    PUBGMO = -1
+  end--
+  
+  function AIM()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 42878508
+    setvalue(so + py, 16, 0)
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 42878816
+    setvalue(so + py, 16, 0)
+    gg.toast("AIMBOT @ANONYMODX")
+  end--
+  
+  function IPAD()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 63055072
+    setvalue(so + py, 16, 260)
+    gg.toast("IPAD VIEW @ANONYMODX")
+  end--
+  
+  function LESS()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 21939444
+    setvalue(so + py, 16, 0)
+    gg.toast("LESS RECOIL  @ANONYMODX")
+  end--
+  
+  function HEAD()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 22219744
+    setvalue(so + py, 16, 0)
+    gg.toast("HEADSHOT @ANONYMODX")
+  end--
+  
+  function SMALL()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 21946952
+    setvalue(so + py, 4, 0)
+    gg.toast("SMALL CROSHIER @ANONYMODX")
+  end--
+  
+  function ANTI()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 21939444
+    setvalue(so + py, 16, 0)
+    py = 62227392
+    setvalue(so + py, 16, 0)
+    py = 32630536
+    setvalue(so + py, 16, 0)
+    py = 62227384
+    setvalue(so + py, 16, 0)
+    py = 23687524
+    setvalue(so + py, 16, 0)
+    py = 63042832
+    setvalue(so + py, 16, 0)
+    gg.toast("NO RECOIL @ANONYMODX")
+  end--
+  
+  function LONG()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 32262824
+    setvalue(so + py, 16, 0)
+    gg.toast("FAST SHOT @ANONYMODX")
+  end--
+  
+  function JUMP()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 19851012
+    setvalue(so + py, 4, -289990142)
+    gg.toast("LONG JUMP @ANONYMODX")
+  end--
+  
+  function KNOCK()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 19850984
+    setvalue(so + py, 16, -2.73959299E28)
+    gg.toast("KNOCK SPEED ON")
+  end--
+  
+  function KNOCKOFF()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 19850984
+    setvalue(so + py, 16, -2.73959284E28)
+    gg.toast("SPEED KNOCK OFF")
+  end--
+  
+  function DESERT()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 56894880
+    setvalue(so + py, 4, 0)
+    gg.toast("DESERT MAP @ANONYMODX")
+  end--
+  
+  function BLACK()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 65899056
+    setvalue(so + py, 4, -1222129996)
+    gg.toast("BLACK SKY @ANONYMODX")
+  end--
+  
+  function PRONE()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 78814652
+    setvalue(so + py, 16, 0)
+    gg.toast("CAR SPEED @ANONYMODX")
+  end--
+  
+  function GRASS()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 42674840
+    setvalue(so + py, 16, 0)
+    gg.toast("NO GRASS @ANONYMODX")
+  end--
+  
+  function FOG()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 51274712
+    setvalue(so + py, 16, 0)
+    gg.toast("NO FOG @ANONYMODX")
+  end--
+  
+  function AIMLOCK()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 67021720
+    setvalue(so + py, 16, 40)
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 42455888
+    setvalue(so + py, 16, 9999999)
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 67021720
+    setvalue(so + py, 4, 100)
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 42455888
+    setvalue(so + py, 16, 9999999)
+    gg.toast("AIMLOCK @ANONYMODX")
+  end--
+  
+  function MAGIC()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 63928868
+    setvalue(so + py, 16, 0)
+    gg.toast("MAGIC @ANONYMODX")
+  end--
+  
+  function INSTANT()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 59845972
+    setvalue(so + py, 16, 0)
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 41012016
+    setvalue(so + py, 4, 0)
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 59846068
+    setvalue(so + py, 16, 0)
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 21854592
+    setvalue(so + py, 16, 0)
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 59846068
+    setvalue(so + py, 16, 0)
+    gg.toast("INSTANT HIT @ANONYMODX ")
+  end--
+  
+  function NIGHT()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 53292320
+    setvalue(so + py, 16, 0)
+    gg.toast("NIGHT MODE @ANONYMODX")
+  end--
+  
+  function XHIT()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 29905592
+    setvalue(so + py, 16, 0)
+    gg.toast("X HIT EFFECT @ANONYMODX")
+  end--
+  
+  function FASTS()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 63427016
+    setvalue(so + py, 16, 0)
+    gg.toast("FLASH ON @ANONYMODX")
+  end--
+  
+  function CARSP()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 63427016
+    setvalue(so + py, 16, -1.86389771E-20)
+    gg.toast("FLASH OFF @ANONYMODX")
+  end--
+  
+  function MO()
+    print("🙋‍♀️ İyi Günler 🙋‍♂️")
+    gg.setVisible(true)
+    os.exit()
+  end--
+  
+  while true do
+    if gg.isVisible(true) then
+      PUBGMO = 1
+      gg.setVisible(false)
+    end--
+    gg.clearResults()
+    if PUBGMO == 1 then
+      HOME()
+    end--
+  end--
+end--
+
+L1_1028.Q()
